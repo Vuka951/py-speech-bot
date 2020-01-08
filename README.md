2abe11624ade37a1cefc8beff17184e0e4a3e9ad
Vuka951 <vultashin76@gmail.com>
2020-01-08 14:22:27 +0100

    added respond functiaonlity and a readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # py-speech-bot
 Listens to you as dies if you tell it to
+Sometimes bugs out since the pyaudio binary isnt official and has issues
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import speech_recognition as sr
+import webbrowser
+import time
 
 r = sr.Recognizer()
 
-def record_audio():
+def record_audio(ask = False):
     with sr.Microphone() as source:
+        if ask:
+            print(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
@@ -14,6 +18,29 @@ def record_audio():
             print('Server Dead!')
         return voice_data
 
+def respond(voice_data):
+    print(f'You said: {voice_data}')
+    if voice_data in ['whatsapp', 'sup']:
+        print('Not much')
+    elif voice_data in ['who you', 'who are you']:
+        print('Ugly Pussy Nigga')
+    elif voice_data in ['time beach', 'time bich', 'time bitch']:
+        print(time.ctime())
+    elif voice_data == 'google':
+        search = record_audio('Google what?')
+        url = f'https://google.com/search?q={search}'
+        webbrowser.get().open(url)
+        print(f'Catch this fade for {search}')
+    elif voice_data == 'find':
+        place = record_audio('Find What?')
+        url = f'https://google.nl/maps/place/{place}/&amp;'
+        webbrowser.get().open(url)
+        print(f'Im sippin tea in {place}, WHAT THE FUKC IS UP')
+    elif voice_data == 'die':
+        exit()
+
 print('Whachu want')
-voice_data = record_audio()
-print(voice_data)
+
+while True:
+    voice_data = record_audio().lower()
+    respond(voice_data)
